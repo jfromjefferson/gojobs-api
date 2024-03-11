5e2ced3a10020c215fa25d81b68136f2fed77d66
Jefferson Silva <jeffsilva1@outlook.com>
2024-03-11 20:30:23 -0300

    feat: Create route logic

--- router/routes.go
+++ router/routes.go
@@ -2,40 +2,20 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/jfromjefferson/gojobs-api/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/jobs", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"message": "GET /jobs",
-			})
-		})
+		v1.GET("/jobs", handler.ListJobHandler)
 
-		v1.GET("/jobs/job", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"message": "GET /jobs/job",
-			})
-		})
+		v1.GET("/jobs/job", handler.GetOneJobHandler)
 
-		v1.POST("/jobs", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"message": "POST /jobs/job",
-			})
-		})
+		v1.POST("/jobs", handler.CreateJobHandler)
 
-		v1.PUT("/jobs/job", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"message": "PUT /jobs/job",
-			})
-		})
+		v1.PUT("/jobs/job", handler.UpdateJobHandler)
 
-		v1.DELETE("/jobs/job", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"message": "DELETE /jobs/job",
-			})
-		})
+		v1.DELETE("/jobs/job", handler.DeleteJobHandler)
 	}
 }
